aaa9ef156c97ac326fdecc7c94586a16051b7fd4
Edwin <giolambo@aol.com>
2019-02-15 18:34:23 -0700

    Update

--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,12 +1,108 @@
 package com.skilldistillery.filmquery.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class DatabaseAccessorObject implements DatabaseAccessor {
 
-  @Override
-  public Film findFilmById(int filmId) {
-    return null;
-  }
+	private static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
 
+	public DatabaseAccessorObject() throws ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+	}
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		DatabaseAccessorObject tester = new DatabaseAccessorObject();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Which film ID would you like to look at ");
+		int userChoice = input.nextInt();
+		tester.findFilmById(userChoice);
+		input.close();
+
+	}
+
+	public Film findFilmById(int filmID) throws SQLException {
+		Film film = null;
+		String user = "student";
+		String pass = "student";
+		Connection conn = DriverManager.getConnection(URL, user, pass);
+		String sql = "SELECT id, title, description FROM film WHERE id= ?";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, filmID);
+		ResultSet filmResult = stmt.executeQuery();
+		if (filmResult.next()) {
+			film = new Film(); // Create the object
+			// Here is our mapping of query columns to our object fields:
+			int id = filmResult.getInt("id");
+			String title = filmResult.getString("title");
+			String description = filmResult.getString("description");
+
+			filmResult.close();
+			stmt.close();
+			conn.close();
+		}
+
+		return film;
+	}
+
+	public Actor findActorById(int actorID) {
+		Actor actor = null;
+		try {
+			String user = "student";
+			String pass = "student";
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+			String sql = "SELECT id,first_name,last_name FROM actor WHERE id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, actorID);
+			ResultSet actorResult = stmt.executeQuery();
+			if (actorResult.next()) {
+				actor = new Actor(); // Create the object
+				// Here is our mapping of query columns to our object fields:
+				actor.setId(actorResult.getInt("id"));
+
+			} else if (!actorResult.next()) {
+				System.out.println("No actor was found");
+			}
+			actorResult.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actor;
+	}
+
+	public List<Actor> findActorsByFilmId(int actorID) throws SQLException {
+		List<Actor> actors = new ArrayList<>();
+		try {
+			Actor actor = null;
+			String user = "student";
+			String pass = "student";
+			Connection conn = DriverManager.getConnection(URL, user, pass);
+			String sql = "SELECT id FROM actor";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, actorID);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				int filmId = rs.getInt("id");
+				Actor Actor1 = new Actor();
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actors;
+	}
 }
